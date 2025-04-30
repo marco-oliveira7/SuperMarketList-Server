e863849082b0d0e35964474c222eca019ef92e86
marco-oliveira7 <ma8155354@gmail.com>
2025-04-29 23:21:08 -0300

    fixed function deleteProductById

--- src/repo/products-repository.ts
+++ src/repo/products-repository.ts
@@ -11,6 +11,16 @@ const database: ProductModel[] = [
     name: "Pimenta",
     isChecked: false,
   },
+  {
+    id: "2",
+    name: "Chocolate",
+    isChecked: false,
+  },
+  {
+    id: "3",
+    name: "Limao",
+    isChecked: false,
+  },
 ];
 
 export const getAllProducts = async () => {
@@ -21,11 +31,20 @@ export const deleteProductById = async (id: string) => {
   const index = database.findIndex((p) => p.id === id);
 
   if (index !== -1) {
-    if (index + 1 !== database.length) {
+    if (index === 0) {
       database?.forEach((product) => {
         let id = parseInt(product.id);
         id--;
         product.id = id.toString();
+      });
+    } else if (index + 1 !== database.length) {
+      // logica para deletar produtos do meio do array
+      database?.slice(index).forEach((product) => {
+        let id = parseInt(product.id);
+        console.log(product)
+        id--;
+        product.id = id.toString();
+        console.log(product)
       });
     }
     database.splice(index, 1);
@@ -52,7 +71,6 @@ export const updateProduct = async (id: string): Promise<string> => {
   let message = "";
   if (checkedProduct) {
     if (checkedProduct.isChecked === true) checkedProduct.isChecked = false;
-
     else checkedProduct.isChecked = true;
 
     message = "Sucesso";
