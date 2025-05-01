cf5753f9c46099305f982ebd0385028f44e1b831
marco-oliveira7 <ma8155354@gmail.com>
2025-05-01 15:43:07 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://market-list-neon.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "DELETE", "PATCH"],
   },
 });
